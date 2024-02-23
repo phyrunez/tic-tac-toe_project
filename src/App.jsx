f13b23852cb485eb9801302b588584c968accaee
Phyrunez <ebukajudee302@gmail.com>
2024-02-23 15:09:45 +0100

    added game board and log of the game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 function App() {
-  
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  const handleSelectBox = (rowIndex, colIndex) => {
+    setActivePlayer(activePlayer => activePlayer === 'X' ? 'O' : 'X' )
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X') currentPlayer = 'O';
+
+      const updatedTurns = [
+        { box: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns
+      ]
+
+      return updatedTurns;
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        <GameBoard onSelectBox={handleSelectBox} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
